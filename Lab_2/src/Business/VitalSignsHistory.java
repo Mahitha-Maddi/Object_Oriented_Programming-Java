d4b96e7ebbe4aa3829a58d69843fa8ee4a53b345
Mahitha-Maddi <maddi.s@husky.neu.edu>
2020-02-02 19:09:01 -0500

    Lab 2 commit

---
+++ Lab_2/src/Business/VitalSignsHistory.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mahit
+ */
+public class VitalSignsHistory {
+    
+    private ArrayList<VitalSigns> vitalSignsHistory;
+
+    public VitalSignsHistory() {
+        this.vitalSignsHistory = new ArrayList<VitalSigns>();
+    }
+    
+    
+
+    public ArrayList<VitalSigns> getVitalSignsHistory() {
+        return vitalSignsHistory;
+    }
+
+    public void setVitalSignsHistory(ArrayList<VitalSigns> vitalSignsHistory) {
+        this.vitalSignsHistory = vitalSignsHistory;
+    }
+    
+    
+    public VitalSigns addVitalSign(){
+    VitalSigns vs=new VitalSigns();
+    vitalSignsHistory.add(vs);
+    return vs;
+    }
+    
+    
+    public void deleteVitalSign(  VitalSigns v){
+ 
+    vitalSignsHistory.remove(v);
+   
+    }
+    
+    
+}
